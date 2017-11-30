a0711584af03143a3bac0d67996dd273684c48b8
Aldrin Navarro <aldrinnavarro16@gmail.com>
2017-11-30 21:46:56 +0800

    Add sample

---
+++ sample/subscribe.py
@@ -0,0 +1,15 @@
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print('Connected. Result code: ', str(rc))
+    client.subscribe('mcutie/init')
+
+def on_message(client, userdata, msg):
+    print('Topic: ', msg.topic, '\nMessage:', msg.payload)
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect('test.mosquitto.org', 1883, 60)
+client.loop_forever()
