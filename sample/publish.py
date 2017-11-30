a9bf34bc3e584c95daa3d40b892fee20cbb8307e
Aldrin Navarro <aldrinnavarro16@gmail.com>
2017-11-30 21:55:18 +0800

    Use localhost instead of live endpoint

--- sample/publish.py
+++ sample/publish.py
@@ -1,6 +1,6 @@
 import paho.mqtt.client as mqtt
 
 mqttc = mqtt.Client('python_pub')
-mqttc.connect('test.mosquitto.org', 1883)
+mqttc.connect('localhost', 1883)
 mqttc.publish('mcutie/init', 'Starting...')
 mqttc.loop(2)
--- sample/subscribe.py
+++ sample/subscribe.py
@@ -11,5 +11,5 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect('test.mosquitto.org', 1883, 60)
+client.connect('localhost', 1883, 60)
 client.loop_forever()
