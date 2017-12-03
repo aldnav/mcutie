06e876c4a5fc13157aecbcf7d911ffd2659c1811
Aldrin Navarro <aldrinnavarro16@gmail.com>
2017-12-03 23:31:52 +0800

    Conform to PEP08

--- sample/subscribe.py
+++ sample/subscribe.py
@@ -1,15 +1,19 @@
 import paho.mqtt.client as mqtt
 
+
 def on_connect(client, userdata, flags, rc):
     print('Connected. Result code: ', str(rc))
     client.subscribe('mcutie/init')
 
+
 def on_message(client, userdata, msg):
     print('Topic: ', msg.topic, '\nMessage:', msg.payload)
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
 
-client.connect('localhost', 1883, 60)
-client.loop_forever()
+if __name__ == '__main__':
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect('localhost', 1883, 60)
+    client.loop_forever()
